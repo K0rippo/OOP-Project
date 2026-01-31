2e984a0a3760f39856bb02f307721a30ad9b1e02
kwankaixian-source <kwan.kaixian@gmail.com>
2026-01-31 17:06:40 +0800

    Addition of IOManager
    Made some edits to GameMaster
    Added IOManager and InputAction

--- core/src/main/java/com/mygdx/game/GameMaster.java
+++ core/src/main/java/com/mygdx/game/GameMaster.java
@@ -1,34 +1,30 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-
-//Elvan's test commit
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class GameMaster extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    
+    private IOManager ioManager;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        ioManager = new IOManager();
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        ScreenUtils.clear(0, 0, 0, 1);
+        
+        if (ioManager != null) {
+            ioManager.handleInput();
+        }
     }
-
+    
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        if (ioManager != null) {
+            ioManager.dispose();
+        }
     }
 }
